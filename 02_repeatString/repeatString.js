73af5418a7519f46c75dffff2a9181000b2071e4
FacuECejas <facuemicejas@gmail.com>
2025-02-07 15:52:51 -0300

    docs: Comments deleted

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -9,13 +9,5 @@ const repeatString = function(string, numOfTimes) {
     return newString;
 };
 
-//La funcion toma dos argumentos, un string y un numero x
-//Devuelve un string que esta formado por el string original repetido x cantidad de veces
-
-//Crear una variable para almacenar el string resultado, inicializarlo en vacío
-//Utilizar un loop que va desde 0 hasta numOfTimes - 1 veces
-    // Concatenar el nuevo string con string argumento
-//Al salir del loop, retornar el nuevo string
-
 // Do not edit below this line
 module.exports = repeatString;
