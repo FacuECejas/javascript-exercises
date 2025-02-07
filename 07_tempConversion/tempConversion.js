035e9dd4e5de8426cad0a8e45ef59d2ffffce967
FacuECejas <facuemicejas@gmail.com>
2025-02-07 18:26:24 -0300

    feat: Completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  
+  let tempInCelsius = (tempInFahrenheit - 32) / (9/5);
+  let rounded = parseFloat(tempInCelsius.toFixed(1));
+
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+
+  let tempInFahrenheit = (tempInCelsius * 1.8) + 32;
+  let rounded = parseFloat(tempInFahrenheit.toFixed(1));
+  
+  return rounded;
 };
 
 // Do not edit below this line
