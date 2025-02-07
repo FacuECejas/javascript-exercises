8eff852bde4cb84929a186ba969614376e1bc773
FacuECejas <facuemicejas@gmail.com>
2025-02-07 15:28:37 -0300

    feat: Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, numOfTimes) {
+    if (numOfTimes < 0) return "ERROR";
 
+    let newString = '';
+    for (let i = 0; i < numOfTimes; i++){
+        newString += string;
+    }
+
+    return newString;
 };
+
+//La funcion toma dos argumentos, un string y un numero x
+//Devuelve un string que esta formado por el string original repetido x cantidad de veces
+
+//Crear una variable para almacenar el string resultado, inicializarlo en vacío
+//Utilizar un loop que va desde 0 hasta numOfTimes - 1 veces
+    // Concatenar el nuevo string con string argumento
+//Al salir del loop, retornar el nuevo string
 
 // Do not edit below this line
 module.exports = repeatString;
