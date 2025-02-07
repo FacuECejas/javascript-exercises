26d5410c6306299bcbe71cac06dee353df471878
FacuECejas <facuemicejas@gmail.com>
2025-02-07 17:46:01 -0300

    feat: Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(array, ...args) {
+
+    let newArray = array.filter((element) => !args.includes(element));
+
+    return newArray;
+
 };
+
+/*
+En este ejercicio usamos rest arguments y el método filter.
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
