edc91d2ab8845beb53cd5bd7558eb84f0dd2b5ae
FacuECejas <facuemicejas@gmail.com>
2025-02-15 00:10:42 -0300

    feat: Completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const removePunctuation = function (string) {
+    let punctuation = /[- _.:,;{\[^~+*¡'?=)(&%$#"!°|¬`\]}]/g
+    return string.replace(punctuation, "");
+}
 
+const palindromes = function (string) {
+    let cleanedStr = removePunctuation(string);
+    let finalStr = cleanedStr.toLowerCase();
+
+    let temp = finalStr.split('');
+    temp.reverse();
+    let reversedStr = temp.join('');
+
+    return reversedStr === finalStr;
 };
+
+/*
+Notar que en la forma en la que lo hice, quizas no cubra todos los signos.
+Por ello, la respuesta dada en "solution" cubre todos los casos realmente.
+Además de que se ve más limpio el código. 
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
