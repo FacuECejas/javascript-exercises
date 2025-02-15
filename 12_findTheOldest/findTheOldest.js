f2058ec49f894d1a7cd84f2e6ace4477cbb99983
FacuECejas <facuemicejas@gmail.com>
2025-02-15 01:05:03 -0300

    feat: Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    
+    array.forEach((person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    })
+
+    array.sort((a, b) => {
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        
+        return bAge - aAge;
+    });
+
+    return array[0];
 
 };
 
