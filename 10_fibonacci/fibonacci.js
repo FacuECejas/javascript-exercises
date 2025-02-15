4fc12b58cff761922a6d198d724f8d10412478f1
FacuECejas <facuemicejas@gmail.com>
2025-02-15 00:24:31 -0300

    feat: Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = Number(number);
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+
+    let first = 1;
+    let second = 1;
+
+    let fibo = 1;
+
+    for (let i = 2; i < number; i++){
+        fibo = first + second;
+        first = second;
+        second = fibo;
+    }
+
+    return fibo;
 
 };
 
