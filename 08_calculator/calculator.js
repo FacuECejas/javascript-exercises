6f8da5f2d316178ee8e3fd1a769592162f2bc52b
FacuECejas <facuemicejas@gmail.com>
2025-02-14 23:45:55 -0300

    feat: Completed exercise 07

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((mul, number) => mul * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1;
+  let result = number * factorial(number - 1);
+
+  return result;
 };
 
 // Do not edit below this line
